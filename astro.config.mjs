f4abb0fa7c8b4980133f55d01f1d27a8be0b46e2
scavro <scavro@protonmail.com>
2025-04-01 20:53:45 +0200

    First commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,22 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, envField } from 'astro/config';
+
+import tailwindcss from '@tailwindcss/vite';
+
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  env:{
+    schema: {
+      SHOW_BUY_BUTTON: envField.boolean ({ default:true , context:'server', access: 'public' }),
+      SCORE_API_ENDPOINT: envField.string({ context:'client', access: 'public' }),
+    }
+  },
+  vite: {
+    plugins: [tailwindcss()]
+  }
+});
+
+
+
+
